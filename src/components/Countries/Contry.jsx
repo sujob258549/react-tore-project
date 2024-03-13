b011c183e9f34bed9660965bf840e04150433291
DevSujon <sujonmaia274@gmail.com>
2024-03-14 05:12:13 +0600

    disjpla

--- src/components/Countries/Contry.jsx
+++ src/components/Countries/Contry.jsx
@@ -1,30 +1,33 @@
 import { useState } from 'react';
 import './style.css'
-const Contry = ({ country }) => {
-    console.log(country)
-    const { name, flags, continents, population,area,subregion,translations,cca3
+const Contry = ({ country, addvisitedCountryName}) => {
+    // console.log(country)
+    const { name, flags, continents, population, area, subregion, translations, cca3
 
 
     } = country;
-    const [Visited , setVisited] = useState(false)
-    const clickBtn = ()=>{
+    const [Visited, setVisited] = useState(false)
+    const clickBtn = () => {
         setVisited(!Visited);
     }
+    // add country name...
     return (
         <div>
-            <div className='country'>
+            <div className={`country ${Visited && 'visited'}`}>
                 <h4>Name : <span style={{ marginLeft: '15px', color: "orange" }}> {name.common}</span></h4>
-                <img src={flags.png} style={{borderRadius: '10px'}} className='image' alt="" />
+                <img src={flags.png} style={{ borderRadius: '10px' }} className='image' alt="" />
                 <p>{continents}</p>
                 <p>Population : {population}</p>
                 <p>Area : {area}</p>
                 <p>cca3 : {cca3}</p>
                 <p>subregion : {subregion}</p>
                 <p>subregion : {translations.ara.common} {translations.ara.official}</p>
-                <button onClick={clickBtn} style={{padding: "10px 20px", marginRight: '20px'}}>Visited</button>
+                <button onClick={clickBtn} style={{ padding: "10px 20px", marginRight: '20px' }}>Visited</button>
                 {
                     Visited ? 'I have visited' : "You will be visited"
                 }
+                <br />
+                <button onClick={() => addvisitedCountryName(country)} style={{ padding: "10px 20px", marginTop: '20px', backgroundColor: 'rgba(222, 184, 135, 0.395)' }}>Visited Area</button>
 
             </div>
         </div>
--- src/components/Countries/Countris.jsx
+++ src/components/Countries/Countris.jsx
@@ -2,21 +2,38 @@ import { useState } from "react";
 import Contry from "./Contry";
 
 const Countris = () => {
-    const[countrys, setCountry] = useState([]);
-    useState(()=>{
+    const [countrys, setCountry] = useState([]);
+    const [visitedCountry, setVisitesCountry] = useState([]);
+    useState(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountry(data));
-        
+            .then(res => res.json())
+            .then(data => setCountry(data));
+
     }, []);
+    const addvisitedCountryName = flags => {
+        const newAddVisitedCountryName = {...visitedCountry, flags};
+        setVisitesCountry(newAddVisitedCountryName);
+    }
     return (
         <div>
-          <h2>country: {countrys.length}</h2>
-         <div className='country-container'>
-         {
-            countrys.map(country => <Contry key={country.cca3} country={country}></Contry>)
-          }
-         </div>
+            <h2>country: {countrys.length}</h2>
+            <h4>Visited Country: {visitedCountry.length} <br />
+                <ol>
+                   {
+                    visitedCountry.map(visit => <li>vi</li>)
+                   }
+                </ol>
+            </h4>
+            <div className='country-container'>
+                {
+                    countrys.map(country => <Contry
+                         key={country.cca3}
+                         addvisitedCountryName={addvisitedCountryName}
+                         country={country}
+                    
+                         ></Contry>)
+                }
+            </div>
         </div>
     );
 };
